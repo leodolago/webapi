91301b92d8393420641ef213dcd1a37293127b08
Leonardo <leodolago@gmail.com>
2021-07-12 09:10:38 -0300

    finished crud

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(db.findUsers());
+});
+
+router.post('/', (req, res) => {
+  const user = db.insertUser(req.body);
+  res.status(201).json(user);
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const user = db.updateUser(id, req.body);
+  res.status(200).json(user);
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  db.deleteUser(id);
+  res.status(200);
+})
+
+module.exports = router;
