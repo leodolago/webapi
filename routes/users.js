18c15ff51abd7bafb818feac8779a6121f57eda7
Leonardo <leodolago@gmail.com>
2021-07-13 09:27:43 -0300

    middleware

--- routes/users.js
+++ routes/users.js
@@ -13,16 +13,25 @@ router.get('/:id', (req, res) => {
   res.json(db.findUser(id));
 })
 
-router.post('/', (req, res) => {
-    const { error } = userSchema.validate(req.body);
-    if(error)
-        return res.status(422).json({error: error.details});
+function validationMiddleware(req, res, next){
+  if(["POST", "PUT"].indexOf(req.method) !== -1){
+    if(!req.body.nome || !req.body.idade)
+    return res.status(422).json({error: "nome and idade are required!"})
+  }
+  const { error } = userSchema.validate(req.body);
+  if(error)
+      return res.status(422).json({error: error.details});
+      else
+      next();
+}
 
+router.post('/', validationMiddleware, (req, res) => {
     const user = db.insertUser(req.body);
     res.status(201).json(user);
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validationMiddleware,
+(req, res) => {
   const id = req.params.id;
   const user = db.updateUser(id, req.body, true);
   res.status(200).json(user);
